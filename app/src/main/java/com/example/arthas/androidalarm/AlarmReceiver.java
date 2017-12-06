2078924344c3e36610fdcaa084a070d89f2c5aca
Fine, Max <wmfine@ilstu.edu>
2017-12-05 18:35:12 -0600

    Pushing changes

--- app/src/main/java/com/example/arthas/androidalarm/AlarmReceiver.java
+++ app/src/main/java/com/example/arthas/androidalarm/AlarmReceiver.java
@@ -7,13 +7,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.widget.Toast;
+import static com.example.arthas.androidalarm.MainActivity.Latitude;
 
 /**
  * Created by Rob on 11/22/2017.
  */
 
-public class AlarmReceiver extends BroadcastReceiver{
+public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
@@ -26,30 +26,30 @@ public class AlarmReceiver extends BroadcastReceiver{
 
 
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder builder = null;
+    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    NotificationCompat.Builder builder = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel notificationChannel = new NotificationChannel("ID", "Name", importance);
-            notificationManager.createNotificationChannel(notificationChannel);
-            builder = new NotificationCompat.Builder(context.getApplicationContext(), notificationChannel.getId());
-        } else {
-            builder = new NotificationCompat.Builder(context.getApplicationContext());
-        }
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel notificationChannel = new NotificationChannel("ID", "Name", importance);
+        notificationManager.createNotificationChannel(notificationChannel);
+        builder = new NotificationCompat.Builder(context.getApplicationContext(), notificationChannel.getId());
+    } else {
+        builder = new NotificationCompat.Builder(context.getApplicationContext());
+    }
 
         System.out.println("why are you not working.");
-        builder = builder
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setWhen(System.currentTimeMillis())
-                .setContentTitle("Location: " + MainActivity.Latitude + " , " + MainActivity.Longitude)
+    builder = builder
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setWhen(System.currentTimeMillis())
+            .setContentTitle("Location: " + Latitude + " , " + MainActivity.Longitude)
                 .setContentText(message)
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setAutoCancel(true)
                 .setContentInfo("Info");
         System.out.println("notification created!");
 
-        final int _id = (int) System.currentTimeMillis();
+    final int _id = (int) System.currentTimeMillis();
 
                 notificationManager.notify(_id, builder.build());
-    }
+}
 }
